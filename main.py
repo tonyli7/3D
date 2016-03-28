fefadf218e4f069c7e371f9f1622476edafa9e10
tonyli7 <xxtonyxxlixx@gmail.com>
2016-03-27 22:14:11 -0400

    added photo

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ color = [ 0, 255, 0 ]
 edges = new_matrix()
 transform = new_matrix()
 f=open("script_3d")
+my_script=open("script_lol")
 if len(sys.argv) == 2:
     f = open(sys.argv[1])
 
-parse_file( f, edges, transform, screen, color )
+#parse_file( f, edges, transform, screen, color )
+parse_file(my_script, edges, transform, screen, color)
 f.close()
