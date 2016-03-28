3ee83e468e1aff94fe6b404c66ed1f1e88960173
tonyli7 <xxtonyxxlixx@gmail.com>
2016-03-27 22:15:13 -0400

    added photo

--- main.py
+++ main.py
@@ -13,6 +13,6 @@ my_script=open("script_lol")
 if len(sys.argv) == 2:
     f = open(sys.argv[1])
 
-#parse_file( f, edges, transform, screen, color )
-parse_file(my_script, edges, transform, screen, color)
+parse_file( f, edges, transform, screen, color )
+#parse_file(my_script, edges, transform, screen, color)
 f.close()
